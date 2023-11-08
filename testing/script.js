d2b464c9981a93c11b20417aa15439cdc7e91602
MuskanGupta-25 <muskangupta15501@gmail.com>
2023-11-09 02:31:53 +0530

    commit

---
+++ testing/script.js
@@ -0,0 +1,11 @@
+document
+
+document.getElementsByClassName('demo')
+
+var para= document.getElementsByClassName('demo')
+
+undefined
+para[0].style.borderColor='blue';
+'blue'
+para[1].style.borderColor='blue';
+'blue'
